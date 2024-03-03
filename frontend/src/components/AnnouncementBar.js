ed0745e53d45526dd493ecb083138b4af337b323
mwysocka <mikafikapika@gmail.com>
2024-03-04 00:46:01 +0100

    Small changes

--- frontend/src/components/AnnouncementBar.js
+++ frontend/src/components/AnnouncementBar.js
@@ -2,18 +2,17 @@ import React from "react";
 import './AnnouncementBar.css';
 
 function AnnouncementBar() {
-    const textItems = [
+    const tableData = [
         'FREE SHIPPING ON BUNDLES & ORDERS OVER $50',
         'FREE SHIPPING ON BUNDLES & ORDERS OVER $50',
         'FREE SHIPPING ON BUNDLES & ORDERS OVER $50',
     ]
-    const textList = textItems.map((text, index) => <p className={`text-center text-zinc-100 marquee-item-${index}`}>{text}</p>);
+    const spanList = tableData.map((text, index) => <span className={`text-center text-zinc-100 marquee-item-${index}`}>{text}</span>);
 
     return (
         <section id="announcement-bar" className="marquee">
             <div className="bg-black text-zinc-100">
-                <span>++++ this is a running text aka marquee using html &amp; css + text length is limited to the width</span>
-                <span>++++ this is a running text aka marquee using html &amp; css + text length is limited to the width</span>
+                {spanList}
             </div>
         </section>
     )
